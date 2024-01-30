cc3c66f431e87ac29db9030dc55c81f5469c51a6
A-Jar-Of-Peanuts <ajarofpeanuts@gmail.com>
2024-01-30 04:04:04 -0800

    more graphing

--- graphing3D.js
+++ graphing3D.js
@@ -4,31 +4,44 @@ var width = 500;
 var height = 500;
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( width, height );
 document.getElementById("three").insertBefore(renderer.domElement, document.getElementById("three").children[1]);
 
 //camera.up.set(0,0,1);
-camera.position.z = 100;
-camera.position.x = 100;
- camera.position.y = 20;
-camera.rotation.z += THREE.MathUtils.degToRad(0);
-camera.rotation.y += THREE.MathUtils.degToRad(20);
-camera.rotation.x += THREE.MathUtils.degToRad(10);
+// camera.position.z = 100;
+// camera.position.x = 100;
+//  camera.position.y = 20;
+// camera.rotation.z += THREE.MathUtils.degToRad(0);
+// camera.rotation.y += THREE.MathUtils.degToRad(20);
+// camera.rotation.x += THREE.MathUtils.degToRad(10);
+var VIEW_ANGLE = 45;
+var ASPECT = width / height;
+var NEAR = 0.1;
+var FAR = 10000;
+var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+camera.up.set(0, 1, 0);
+camera.lookAt(0, 0, 0);
+camera.position.z = 200;
+camera.position.y = 25;
+camera.position.x = 25;
+// camera.rotation.y += THREE.MathUtils.degToRad(10);
+// camera.rotation.x += THREE.MathUtils.degToRad(10);
+scene.add(camera);
 
 
 const vertices = [];
 
-for ( var i = -20; i<=10; i+=.1) {
+for ( var i = -20; i<=10; i+=.05) {
 	
-    for (var j = -10; j<= 10; j+=.1) {
+    for (var j = -10; j<= 10; j+=.05) {
         var x = i;
         var y = j;
-        var z = x*x;
+        var z = y*y+x*x;
         //var z = y*y;
-        vertices.push( x, y, z );
+        vertices.push( x, z, y );
     }
 }
 
@@ -40,7 +53,7 @@ scene.add( points );
 
 function axes() {
 
-    const axesHelper = new THREE.AxesHelper( 50 );
+    const axesHelper = new THREE.AxesHelper( 10000 );
     scene.add( axesHelper );
 }
 
